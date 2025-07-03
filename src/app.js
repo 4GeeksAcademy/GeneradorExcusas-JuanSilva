b1e2aa2770f90ff585de3b1d3c8d64f931cfd937
Juan Silva <162917944+juancapanegra@users.noreply.github.com>
2025-07-03 17:56:55 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
-import "bootstrap";
 import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+  function generateExcuse() {
+
+    const who = ['El perro', 'Mi abuela', 'El cartero', 'Mi pájaro', 'Mi amigo', 'Mi vecino'];
+    const action = ['comió', 'orinó', 'aplastó', 'rompió','perdió', 'vendió'];
+    const what = ['mi tarea', 'mi telefono', 'el coche', 'la mesa', 'el proyecto']; 
+    const when = ['antes de la clase', 'mientras dormía', 'mientras hacía ejercicio', 'durante la comida', 'mientras programaba'];  
+
+    function randomWho() {
+      const randomNumberWho = Math.floor(Math.random() * who.length);
+      return randomNumberWho;
+    } 
+
+    function randomAction() {
+      const randomNumberAction = Math.floor(Math.random() * action.length);
+      return randomNumberAction;
+    }
+
+    function randomWhat() {
+      const randomNumberWhat = Math.floor(Math.random() * what.length);
+      return randomNumberWhat;
+    }
+    
+    function randomWhen() { 
+      const randomNumberWhen = Math.floor(Math.random() * when.length);
+      return randomNumberWhen;
+    } 
+
+    return who[randomWho()] + " " + action[randomAction()] + " " + what[randomWhat()] + " " + when[randomWhen()];
+  }
+
+  const excuse = generateExcuse();  
+
+  document.getElementById("excuse").innerHTML = excuse; 
+}  
+
